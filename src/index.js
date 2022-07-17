c37953c460139bb001abf9440d967e4980b48985
sesychev <sergeysychou@gmail.com>
2022-07-17 23:16:40 +0300

    feat: finish brackets

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   let array = [];
-
+  let top = 0;
+  //just for fun
   for (const bracket of str) {
     for (const item of bracketsConfig) {
-      if (bracket === item[0]) {
+      if (bracket === item[0] && bracket !== item[1]) {
         array.push(bracket);
-      }
-      if (bracket === item[1]) {
+        top++;
+      } else if (bracket === item[1]) {
         const element = array.pop();
-        if (element !== item[0]) {
+        if (element === item[0]) {
+          top--;
+        } else if (element) {
+          array.push(element);
           array.push(bracket);
+          top++;
+        } else {
+          array.push(bracket);
+          top++;
         }
       }
     }
+    console.log(array);
   }
 
   return array.length === 0;
